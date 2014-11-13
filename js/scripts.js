a82966ae2ba8a41a67aece73cce472fb121aacb2
Hasiba <hasiba_arshad@hotmail.com>
2014-11-12 22:09:12 -0800

    Added scroll animations & arrow down anim

--- js/scripts.js
+++ js/scripts.js
@@ -2,50 +2,9 @@ $(document).ready(function() {
 
 	var $window = $(window);
 	
-/*
-	$('*[data-scroll="white"]').each(function(){
-		var $scrollEle = $(this); 
-		
-		// $scrollEle.css({position:'relative'}); // Messes up the spacing on everything
-		
-		var $cover = $('<div class="whiteCover"></div>');
-		$scrollEle.append($cover);
-		
-		var calculateOpacity = function() {
-			
-			var scrollY = $window.scrollTop();
-			var elementY = parseFloat($scrollEle.offset().top);
-			var elementH = $scrollEle.height();
-			var scrollY2 = scrollY + $(window).height();
-						
-			
-			$cover.height(scrollY2 - elementY);
-			var scrolledAmount = $window.scrollTop() / $(window).height()
-			var opacity = 1 - scrolledAmount - Math.floor(scrolledAmount);
-			
-			// Start turning back into color after 1 full screen has been scrolled
-			if($window.scrollTop() > $(window).height()) {
-				opacity = scrolledAmount - Math.floor(scrolledAmount) /2;
-			}
-			// Stay solid color after 2 screens of scrolling
-			if(Math.floor(scrolledAmount) > 1) {
-				opacity = 1;
-			}		
-			return opacity;
-		};
-		
-		$(window).scroll(function() {	
-			$scrollEle.css({ opacity: calculateOpacity() });
-		});
-		$(window).resize(function(e) {
-			$scrollEle.css({ opacity: calculateOpacity() });
-		});
-	});*/
-	
-	
-	
-	// Working but breaks when zoomed out
-	
+	/*************************************
+		Opacity parallax
+	*************************************/
 	$('*[data-scroll="white"]').each(function(){
 		var $scrollEle = $(this); 
 		
@@ -58,12 +17,9 @@ $(document).ready(function() {
 			var scrolledAmount = $window.scrollTop() / $(window).height();
 			var opacity = 1 - scrolledAmount - Math.floor(scrolledAmount); // Fade out
 			
-			// Start turning back into color after 1 full screen has been scrolled
-			
-			console.log($window.scrollTop());
+			// Start turning back into color after 1 full screen has been scrolled			
 			if($window.scrollTop() > $(window).height() + 100) { // Normal
 				opacity = scrolledAmount - Math.floor(scrolledAmount) /2;
-				console.log("hit");
 			}
 			
 			// Show the down link only as long as we haven't scrolled past 1/3 of the screen height
@@ -90,19 +46,23 @@ $(document).ready(function() {
 	
 
 	
-
 	
+	/*************************************
+		Move elements parallax
+	*************************************/	
 	$('*[data-scroll="down"]').each(function(){
 		// alert("Hi");
 		var $scrollEle = $(this); 
 		$(window).scroll(function() {
 			var yPos = -($window.scrollTop() / 20);
 			
-			
+			/* console.log(yPos + " " + $window.scrollTop());		 */
 			
 			if($window.scrollTop() > $(window).height()) {	
 				yPos = -$(window).height()*2;
 			}
+			
+			// Weird bug: sometimes scrolling too fast will push the page down, leaving white space
 			
 			$scrollEle.css({
 				top: yPos + 'px'
@@ -111,6 +71,10 @@ $(document).ready(function() {
 	});
 
 	
+	
+	/*************************************
+		Menu animation for small screens
+	*************************************/
 	$('#menuDrawer').on('click', function(e) {
 		e.preventDefault();
 		if($('nav .right').css('display') == 'none') {
@@ -147,6 +111,26 @@ $(document).ready(function() {
 		}
 	});
 	
+	
+	/*************************************
+		Smooth Scrolling
+		http://css-tricks.com/snippets/jquery/smooth-scrolling/
+	*************************************/
+	$('a[href*=#]:not([href=#])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+			|| location.hostname == this.hostname) {
+
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			   if (target.length) {
+				 $('html,body').animate({
+					 scrollTop: target.offset().top
+				}, 1000);
+				return false;
+			}
+		}
+	});
+		
 	
 
 });
